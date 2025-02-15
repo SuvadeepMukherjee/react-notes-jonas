bfec172310738e987072cc1160e8f13afc655e67
Suvadeep <suvadeepm0709@gmail.com>
2025-02-15 19:27:23 +0530

    47-Conditional rendering with && operator

--- Part-1/pizza-menu/src/index.js
+++ Part-1/pizza-menu/src/index.js
@@ -24,14 +24,19 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  //const pizzas = [];
+  const numPizzas = pizzas.length;
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzas.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
     </main>
   );
 }
@@ -62,7 +67,12 @@ function Footer() {
   // else alert("Sorry we're closed");
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()} We're currently open{" "}
+      {isOpen && (
+        <div className="order">
+          <p>We're open until {closeHour}:00 .Comw visit us or order online</p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
   //return React.createElement("footer", null, "We're currently open!");
